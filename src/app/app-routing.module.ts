b75c50a0ad938a3fe1f6233d4ea8e0e7eedc3156
Akhil Kumar <akhil_kumar@Akhil-MacBook-Air-9.local>
2020-09-06 21:03:13 +0530

    added error page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'layout' },
   { path: 'layout', loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule) },
+  { path: '**', component:ErrorComponent },
 ];
 
 @NgModule({
